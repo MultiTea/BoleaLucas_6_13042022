191526048cc597ea04fcbf3ed6976b0ee590b909
MultiTea <lucasbolea@live.fr>
2022-05-04 10:27:42 +0200

    Backend architecture

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require ('express')
+const app= express()
+const mongoose = require ('mongoose')
+require('dotenv').config();
+
+const path = require ('path')
+
+const userRoutes = require('./routes/user')
+const saucesRoutes = require('./routes/sauces')
+const likeRoutes = require('./routes/like')
+
+mongoose.connect(`${process.env.MONGO_URI}`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json())
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use('/api/auth', userRoutes)
+app.use('/api/sauces', saucesRoutes)
+app.use('/api/sauces', likeRoutes)
+
+module.exports = app
---
+++ backend/controllers/like.js
@@ -0,0 +1,51 @@
+const Sauce = require('../models/Sauce') // sauceSchema
+
+// un user peut like, dislike, ou cancel
+exports.likeStatus = (req, res) => {
+	const like = req.body.like
+	const userId = req.body.userId
+
+	// on cherche la sauce sélectionnée
+	Sauce.findOne({ _id: req.params.id })
+		.then((sauce) => {
+			// find : vérifie si userId existe déjà
+			let userLike = sauce.usersLiked.find((id) => id === userId)
+			let userDislike = sauce.usersDisliked.find((id) => id === userId)
+
+			console.log('Statut : ', like);
+
+			// case "n" : n valeur au click
+			// += : j'ajoute || -= je retire 
+			// filter : montre les id différents de l'userId
+			switch (like) {
+				// like +1
+				case 1:
+					sauce.likes += 1
+					sauce.usersLiked.push(userId)
+				break
+
+				// cancel -1
+				case 0:
+					if (userLike) {
+						sauce.likes -= 1
+						sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId)
+					}
+					if (userDislike) {
+						sauce.dislikes -= 1
+						sauce.usersDisliked = sauce.usersDisliked.filter((id) => id !== userId)
+					}
+				break
+
+				// dislike +1
+				case -1:
+					sauce.dislikes += 1
+					sauce.usersDisliked.push(userId)
+			}
+			// sauvegarde la sauce avec like/dislike
+			sauce
+				.save()
+				.then(() => res.status(201).json({ message: 'save sauce' }))
+				.catch((error) => res.status(400).json({ error }))
+		})
+		.catch((error) => res.status(500).json({ error }))
+}
