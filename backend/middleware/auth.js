c7eb47a9130d62bb3fcef996a7136f0b12f22909
MultiTea <lucasbolea@live.fr>
2022-05-04 10:01:17 +0200

    Adding backend api structure, testing account

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(' ')[1]
+		// la fonction verify décode notre token
+		const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET')
+		// on extrait l'ID user de notre token
+		const userId = decodedToken.userId
+		req.auth = { userId }
+		if (req.body.userId && req.body.userId !== userId) {
+			throw 'Invalid user ID'
+		} else {
+			next()
+		}
+	} catch {
+		res.status(401).json({
+			error: new Error('Invalid request!'),
+		})
+	}
+}
